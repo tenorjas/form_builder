1d73554bb2a060e8a75b32dec1f3e3b84fb83d3c
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-07-11 17:17:30 -0400

    close enough for government work

--- main.js
+++ main.js
@@ -4,97 +4,122 @@ let formData = [
   // the first form element has been added to the HTML file as a comment
   // compare the input in the HTML file with the contents of this first object
   {
-    "type": "text",
-    "label": "First Name",
-    "id": "user-first-name",
-    "icon": "fa-user",
-    "options": []
+    type: "text",
+    label: "First Name",
+    id: "user-first-name",
+    icon: "fa-user",
+    options: []
   },
   {
-    "type": "text",
-    "label": "Last Name",
-    "id": "user-last-name",
-    "icon": "fa-user",
-    "options": []
+    type: "text",
+    label: "Last Name",
+    id: "user-last-name",
+    icon: "fa-user",
+    options: []
   },
   {
-    "type": "email",
-    "label": "Email Address",
-    "id": "user-email",
-    "icon": "fa-envelope",
-    "options": []
+    type: "email",
+    label: "Email Address",
+    id: "user-email",
+    icon: "fa-envelope",
+    options: []
   },
   {
-    "type": "text",
-    "label": "Current Website URL",
-    "id": "user-website",
-    "icon": "fa-globe",
-    "options": []
+    type: "text",
+    label: "Current Website URL",
+    id: "user-website",
+    icon: "fa-globe",
+    options: []
   },
   {
-    "type": "select",
-    "label": "Select Language",
-    "id": "user-language",
-    "icon": "",
-    "options": [
+    type: "select",
+    label: "Select Language",
+    id: "user-language",
+    icon: "",
+    options: [
       {
-        "label": "English",
-        "value": "EN"
+        label: "English",
+        value: "EN"
       },
       {
-        "label": "French",
-        "value": "FR"
+        label: "French",
+        value: "FR"
       },
       {
-        "label": "Spanish",
-        "value": "SP"
+        label: "Spanish",
+        value: "SP"
       },
       {
-        "label": "Chinese",
-        "value": "CH"
+        label: "Chinese",
+        value: "CH"
       },
       {
-        "label": "Japanese",
-        "value": "JP"
+        label: "Japanese",
+        value: "JP"
       }
     ]
   },
   {
-    "type": "textarea",
-    "label": "Your Comment",
-    "id": "user-comment",
-    "icon": "fa-comments",
-    "options": []
+    type: "textarea",
+    label: "Your Comment",
+    id: "user-comment",
+    icon: "fa-comments",
+    options: []
   },
   {
-    "type": "tel",
-    "label": "Mobile Number",
-    "id": "user-mobile",
-    "icon": "fa-mobile-phone",
-    "options": []
+    type: "tel",
+    label: "Mobile Number",
+    id: "user-mobile",
+    icon: "fa-mobile-phone",
+    options: []
   },
   {
-    "type": "tel",
-    "label": "Home Number",
-    "id": "user-phone",
-    "icon": "fa-phone",
-    "options": []
+    type: "tel",
+    label: "Home Number",
+    id: "user-phone",
+    icon: "fa-phone",
+    options: []
   }
 ];
 
 // HINTS:
 // As you can see, we access the first element in the array
 // with [0] and then grab the property "label" using the "." operator
-( function(){
+(function() {
   // Select the first element from the array
-  let first = formData[ 0 ];
+  let first = formData[0];
   // Log the first object
-  console.log( first );
+  console.log(first);
   // Log the string "First Name"
-  console.log( first.label );
-} )();
-
+  console.log(first.label);
+})();
 
 // -------- Your Code Goes Below this Line --------
+let fields = document.querySelector("#fields");
 
+for (var i = 0; i < formData.length; i++) {
+  if (formData[i].type === "select") {
+    let selectLanguage = [
+      "English",
+      "French",
+      "Spanish",
+      "Chinese",
+      "Japanese"
+    ];
+    let formSelect = document.createElement("select");
+    let opt = document.createElement("option");
+    opt.textContent = "Select Language";
 
+    for (var j = 0; j < formData[i].options.length; j++) {
+      let langOpt = document.createElement("option");
+      langOpt.textContent = selectLanguage[j];
+      formSelect.appendChild(langOpt);
+    }
+    fields.appendChild(formSelect);
+  } else {
+    let formInput = document.createElement("input");
+    formInput.type = formData[i].type;
+    formInput.placeholder = formData[i].label;
+    fields.appendChild(formInput);
+  }
+}
